1a41bc50e5f03f3ae2b05190b421b3ee39a0bc3a
Asmaa Albermawy <asmaa.albermawy@gmail.com>
2020-05-04 17:46:31 +0200

    Added my project

---
+++ src/app/app.module.ts
@@ -0,0 +1,80 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+//agendaprog
+import { FormsModule } from '@angular/forms';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_components';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+
+//sociallogin
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+
+const config = new AuthServiceConfig([
+    {
+      id: GoogleLoginProvider.PROVIDER_ID,
+      provider: new GoogleLoginProvider('862895939324-v05el3s26cb2a1a7lagcsmg6bmrngu61.apps.googleusercontent.com')
+    },
+    //{
+      //id: FacebookLoginProvider.PROVIDER_ID,
+      //provider: new FacebookLoginProvider("Facebook-App-Id")
+    //}
+  ]);
+   
+  export function provideConfig() {
+    return config;
+  }
+   
+ 
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing,
+        SocialLoginModule,
+        FormsModule,
+        RouterModule
+
+
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+   
+        
+    ],
+    providers: [
+     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        {
+         provide: AuthServiceConfig,
+          useFactory: provideConfig
+         },
+
+       //  provider used to create fake backend
+        fakeBackendProvider
+    ],
+   
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,84 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AlertService, UserService, AuthenticationService } from '@/_services';
+
+//social media
+import { AuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider } from 'angularx-social-login';
+
+
+@Component({templateUrl: 'register.component.html'})
+export class RegisterComponent implements OnInit {
+    registerForm: FormGroup;
+    loading = false;
+    submitted = false;
+    user: SocialUser;
+
+ 
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private userService: UserService,
+        private alertService: AlertService,
+        private authService: AuthService
+
+    ) { 
+        // redirect to home if already logged in
+        if (this.authenticationService.currentUserValue) { 
+            this.router.navigate(['/']);
+        }
+    }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            username: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+        //social
+        this.authService.authState.subscribe((user) => {
+            this.user = user;
+            console.log(user);
+          });
+    }    
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.userService.register(this.registerForm.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+    signInWithGoogle(): void {
+        this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(x => console.log(x));
+      }
+
+      signOut(): void {
+        this.authService.signOut();
+      }
+
+}
+
